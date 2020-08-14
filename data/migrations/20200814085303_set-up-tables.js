449ef987435b1b15d64d2e63818beef55c924b49
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-08-14 09:27:11 -0700

    set up tables

--- data/migrations/20200814085303_set-up-tables.js
+++ data/migrations/20200814085303_set-up-tables.js
@@ -1,8 +1,48 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('projects', (table) => {
+      table.increments('id').unique();
 
-exports.up = function(knex) {
-  
+      table.string('name', 255).notNullable();
+      table.text('description');
+      table.boolean('completed').notNullable().defaultTo('false');
+    })
+    .createTable('project_resources', (table) => {
+      table.increments('id').unique();
+
+      table
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects');
+
+      table
+        .integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('resources');
+    })
+    .createTable('resources', (table) => {
+      table.increments('id').unique();
+
+      table.string('name', 255).notNullable();
+      table.text('desciption');
+    })
+    .createTable('tasks', (table) => {
+      table.increments('id').unique();
+
+      table.text('description').notNullable();
+      table.text('notes');
+      table.boolean('completed').notNullable().defaultTo('false');
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('projects');
 };
